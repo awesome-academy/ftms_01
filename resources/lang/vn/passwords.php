8b0711ed7e2c881f1f022da9388cb236e349dc78
Nguyen Tien Phi <phintqb@gmail.com>
2018-12-28 11:01:49 +0700

    I18n

---
+++ resources/lang/vn/passwords.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'password' => 'Mật khẩu chứa ít nhất 6 ký tự và đúng với mật khẩu xác nhận',
+        'reset' => 'Mật khẩu của bạn đã được cập nhật!',
+        'sent' => 'Chúng tôi đã gửi email liên kết đặt lại mật khẩu!',
+        'token' => 'Mã xác nhận không đúng.',
+        'user' => 'Chúng tôi không tìm thấy người dùng có địa chỉ email đó!',
+    ];
